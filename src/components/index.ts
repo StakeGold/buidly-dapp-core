b6cc1ae95532bb9061d25c8c889dfbe41863114e
Gabriel Matei <gabrielvmatei@gmail.com>
2022-12-11 19:53:24 +0200

    update

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,2 @@
 export { default as ActionOrConnect } from './ActionOrConnect';
 export { default as Spinner } from './Spinner';
-export { default as Steps } from './Steps';
-export { default as TextField } from './TextField';
-export { default as TooltipSlider } from './TooltipSlider';
